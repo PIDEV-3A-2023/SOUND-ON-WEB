45c08f6622a3c8c2f353028f7674c49a6b10c351
siwar.ahmadi@esprit.tn <siwar.ahmadi@esprit.tn>
2023-04-08 00:51:19 +0100

    CRUD+DASHBOARD

--- src/Form/AlbumType.php
+++ src/Form/AlbumType.php
@@ -2,25 +2,26 @@
 
 namespace App\Form;
 
-use App\Entity\Categorie;
+use App\Entity\Album;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class CategorieType extends AbstractType
+class AlbumType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('nom')
-           
+            ->add('dateCreation')
+            ->add('idUser')
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Categorie::class,
+            'data_class' => Album::class,
         ]);
     }
 }
