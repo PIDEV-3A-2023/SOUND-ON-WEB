43ed3b45ce766b38f3d0823db15eb9dea29ba8f7
Ranim222 <ranim.ahmadi@esprit.tn>
2023-04-14 00:08:22 +0100

    last

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -56,7 +56,7 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
              return new RedirectResponse($this->urlGenerator->generate('app_back_office_admin_dashboard'));
         }
         else 
-         return new RedirectResponse($this->urlGenerator->generate('app_home'));
+         return new RedirectResponse($this->urlGenerator->generate('app_account'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
