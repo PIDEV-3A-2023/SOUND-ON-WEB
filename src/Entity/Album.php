a13ccc2bd12059c40a5a7e3386eedafd8c6c7ad7
siwar <siwar.mejri@esprit.tn>
2023-04-14 03:13:20 +0200

    commit

--- src/Entity/Album.php
+++ src/Entity/Album.php
@@ -6,7 +6,7 @@ use App\Repository\AlbumRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
-use Doctrine\ORM\Mapping as ORM;  
+use Doctrine\ORM\Mapping as ORM;     
 
 #[ORM\Entity(repositoryClass: AlbumRepository::class)]
 class Album
