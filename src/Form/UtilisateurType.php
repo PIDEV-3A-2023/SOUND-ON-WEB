d2ab95d28aa2fedb9cf032bd8eacf9b1df2f09d3
Ranim222 <ranim.ahmadi@esprit.tn>
2023-04-16 16:03:53 +0100

    update

--- src/Form/UtilisateurType.php
+++ src/Form/UtilisateurType.php
@@ -4,17 +4,32 @@ namespace App\Form;
 
 use App\Entity\Utilisateur;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UtilisateurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'constraints' => new NotBlank(),
+                'constraints' => new Length([
+                    'min' => 2 ,
+                    'max' => 30
+                ]),
+                
+                'attr' => [
+                    'placeholder' => 'Saisir email'
+                ]
+            ])
             ->add('roles', ChoiceType::class, [
                 'required' => true,
                 'multiple' => false,
@@ -25,9 +40,42 @@ class UtilisateurType extends AbstractType
                 ],
             ])
             ->add('password')
-            ->add('nom')
-            ->add('prenom')
-            ->add('login')
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => new NotBlank(),
+                'constraints' => new Length([
+                    'min' => 2 ,
+                    'max' => 30
+                ]),
+               
+                'attr' => [
+                    'placeholder' => 'Saisir votre nom'
+                ]
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Nom',
+                'constraints' => new NotBlank(),
+                'constraints' => new Length([
+                    'min' => 2 ,
+                    'max' => 30
+                ]),
+               
+                'attr' => [
+                    'placeholder' => 'Saisir  prenom'
+                ]
+            ])
+            ->add('login', TextType::class, [
+                'label' => 'Login',
+                'constraints' => new NotBlank(),
+                'constraints' => new Length([
+                    'min' => 2 ,
+                    'max' => 30
+                ]),
+                
+                'attr' => [
+                    'placeholder' => 'Saisir votre login'
+                ]
+            ])
         ;
 
         $builder->get('roles')
