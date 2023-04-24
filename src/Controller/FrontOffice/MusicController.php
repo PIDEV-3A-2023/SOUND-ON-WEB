2f41ead959f9d76ca37e2761ef00566f0c5777dd
Nassim BenSalah <nassim.bensalah@esprit.tn>
2023-04-24 22:29:50 +0100

    music now works, but audio controls not displaying

--- src/Controller/FrontOffice/MusicController.php
+++ src/Controller/FrontOffice/MusicController.php
@@ -5,6 +5,7 @@ namespace App\Controller\FrontOffice;
 use App\Entity\Musique;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/music')]
@@ -24,6 +25,27 @@ class MusicController extends AbstractController
         return $this->render('front_office/music/show.html.twig', [
             'song' => $musique,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_front_office_music_play')]
+    public function play(Musique $musique): Response
+    {
+        return $this->render('home/music-player.html.twig', [
+            'song' => $musique,
+        ]);
+    }
+
+    #[Route('/file/{id}', name: 'app_front_office_music_content')]
+    public function songContent(Musique $musique): Response
+    {
+        $file = $musique->getChemin();
+        $response = new Response();
+        $disposition = $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_INLINE, basename($musique->getChemin()));
+        $response->headers->set('Content-Disposition', $disposition);
+        $response->headers->set('Content-Type', 'audio/mpeg');
+        $response->setContent(file_get_contents($file));
+
+        return $response;
     }
 
 }
